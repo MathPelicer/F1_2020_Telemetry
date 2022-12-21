fcf1939d19ff956f327a3943aa48fb2441e072c5
MathPelicer <matheuspelicer58@gmail.com>
2022-12-20 22:22:08 -0300

    Add project files.

---
+++ udp_f1_data/Models/CarMotionData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udp_f1_data.Models
+{
+    public struct CarMotionData
+    {
+        float m_worldPositionX;           // World space X position
+        float m_worldPositionY;           // World space Y position
+        float m_worldPositionZ;           // World space Z position
+        float m_worldVelocityX;           // Velocity in world space X
+        float m_worldVelocityY;           // Velocity in world space Y
+        float m_worldVelocityZ;           // Velocity in world space Z
+        int m_worldForwardDirX;         // World space forward X direction (normalised)
+        int m_worldForwardDirY;         // World space forward Y direction (normalised)
+        int m_worldForwardDirZ;         // World space forward Z direction (normalised)
+        int m_worldRightDirX;           // World space right X direction (normalised)
+        int m_worldRightDirY;           // World space right Y direction (normalised)
+        int m_worldRightDirZ;           // World space right Z direction (normalised)
+        float m_gForceLateral;            // Lateral G-Force component
+        float m_gForceLongitudinal;       // Longitudinal G-Force component
+        float m_gForceVertical;           // Vertical G-Force component
+        float m_yaw;                      // Yaw angle in radians
+        float m_pitch;                    // Pitch angle in radians
+        float m_roll;                     // Roll angle in radians   
+    }
+}
---
+++ udp_f1_data/Program.cs
@@ -0,0 +1,56 @@
+﻿using Codemasters.F1_2020;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace udp_f1_data
+{
+    class UDPListener
+    {
+        private const int listenPort = 20777;
+
+        private static void StartListening()
+        {
+            UdpClient udpClient = new UdpClient(listenPort);
+            IPEndPoint groupEp = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                Console.WriteLine("waiting for data...");
+                while (true)
+                {
+                    byte[] bytes = udpClient.Receive(ref groupEp);
+                    var pt = CodemastersToolkit.GetPacketType(bytes);
+
+                    if (pt.ToString() == "CarTelemetry")
+                    {
+                        TelemetryPacket tp = new TelemetryPacket();
+                        var playerIndex = tp.PlayerCarIndex;
+                        tp.LoadBytes(bytes);
+                        Console.Clear();
+
+                        //foreach (TelemetryPacket.CarTelemetryData ctd in tp.FieldTelemetryData)
+                        //{
+
+                        Console.WriteLine(tp.FieldTelemetryData[playerIndex].SpeedKph.ToString());
+                        //}     
+                    }
+                    
+                }
+            }
+            catch(SocketException exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+        }
+        public static void Main()
+        {
+            StartListening();
+        }
+    }
+}
